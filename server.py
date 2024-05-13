d5ba6eeb72fcc7d34a7ece3cc609002cd44e95f4
Utkarsh Sanjivan <utkarsh_sanjivan@condenast.com>
2024-05-14 00:11:25 +0530

    feat(summarize): added the support for PDF summarize using Bert

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from summarizer import Summarizer
 from flask_cors import CORS
-import PyPDF2
+from PyPDF2 import PdfReader
 
 app = Flask(__name__)
 CORS(app)
@@ -10,6 +10,7 @@ bert_model = Summarizer()
 
 @app.route('/summarize', methods=['POST'])
 def summarize():
+    print('Inside summarize')
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'})
 
@@ -17,21 +18,14 @@ def summarize():
     if file.filename == '':
         return jsonify({'error': 'No selected file'})
 
-    if file and file.filename.endswith('.pdf'):
-        pdf_text = extract_text_from_pdf(file)
-        summarized_text = bert_summarize(pdf_text)
-        return jsonify({'summary': summarized_text})
-    else:
-        return jsonify({'error': 'Unsupported file format'})
-
-def extract_text_from_pdf(file):
-    pdf_text = ''
-    with open(file.filename, 'rb') as f:
-        reader = PyPDF2.PdfFileReader(f)
-        for page_num in range(reader.numPages):
-            page = reader.getPage(page_num)
-            pdf_text += page.extractText()
-    return pdf_text
+    pdf_reader = PdfReader(file)
+    text = ''
+    for page in pdf_reader.pages:
+        print(page)
+        text += page.extract_text()
+    
+    summarized_text = bert_summarize(text)
+    return summarized_text
 
 def bert_summarize(text):
     return ''.join(bert_model(text, min_length=60))
